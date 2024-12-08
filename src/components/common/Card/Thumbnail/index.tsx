94e943e5d55e2f532d4aa108d6494e20ae4776ce
Bagas Ranggen <bagas.ranggen@gmail.com>
2024-12-08 16:37:00 +0700

    feature: add card components

---
+++ src/components/common/Card/Thumbnail/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { ArrayString } from '@/libs/@types';
+import { joinArrayString } from '@/libs/utils';
+
+import Base, { BaseProps as CardBaseProps } from '@/components/common/Card/Base';
+import Button, { BaseAnchorProps } from '@/components/common/Button';
+import Picture, { BaseProps } from '@/components/common/Picture';
+
+export type ThumbnailItemProps = {
+    cta: Omit<BaseAnchorProps, 'as'>;
+    media: BaseProps['items'];
+    title: string;
+    price?: string;
+};
+
+export type ThumbnailProps = {
+    items: ThumbnailItemProps[];
+} & Omit<CardBaseProps, 'items'>;
+
+const Thumbnail = ({ items, width = 3, ...props }: ThumbnailProps): React.ReactElement => {
+    return (
+        <Base
+            {...props}
+            width={width}
+            items={items.map(({ cta, media, title, price }: ThumbnailItemProps) => {
+                const hasPrice = !!price;
+
+                let titleClass: ArrayString = ['mt-1'];
+                if (hasPrice) titleClass.push('text-[1.4rem]');
+                if (!hasPrice) titleClass.push('uppercase font-semibold tracking-[.2rem]');
+                titleClass = joinArrayString(titleClass);
+
+                let priceBlock = undefined;
+                if (hasPrice) {
+                    priceBlock = (
+                        <div className="flex align-baseline mt-1 gap-x-[.2rem]">
+                            <span className="text-sm">RP</span>
+                            <span className="text-[3rem] leading-[2.5rem] font-semibold">{price}</span>
+                        </div>
+                    );
+                }
+
+                return {
+                    children: (
+                        <Button
+                            as="anchor"
+                            {...cta}>
+                            <Picture items={media} />
+                            <h3 className={titleClass}>{title}</h3>
+                            {priceBlock}
+                        </Button>
+                    ),
+                };
+            })}
+        />
+    );
+};
+
+export default Thumbnail;
