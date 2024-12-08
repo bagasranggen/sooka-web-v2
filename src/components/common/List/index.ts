1b4f4a85e75465c2c844e7a2eb819cb138f37070
Bagas Ranggen <bagas.ranggen@gmail.com>
2024-12-08 16:37:09 +0700

    feature: add list component

---
+++ src/components/common/List/index.ts
@@ -0,0 +1,7 @@
+import { Component } from '@/libs/@types';
+
+import Base, { BaseProps } from '@/components/common/List/Base';
+
+export type * from '@/components/common/List/Base';
+
+export default Object.assign<Component<BaseProps>, {}>(Base, {});
