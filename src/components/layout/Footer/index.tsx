e3a222409786983ad4d74a9e0dba5660c5a3bf06
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-12-19 17:23:22 +0700

    refactor: add react icons

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import * as CiIcon from 'react-icons/ci';
+
 import Icon from '@/components/common/Icon';
 import Container from '@/components/common/Container';
 import Columns from '@/components/common/Columns';
@@ -20,8 +22,18 @@ const Footer = ({}: FooterProps): React.ReactElement => {
                         <Columns.Column
                             width={{ md: 6 }}
                             className="*:!mb-0 uppercase tracking-0.2 font-semibold text-[1.2rem] text-center lg:text-left">
-                            <p>Sleman, Daerah Istimewa Yogyakarta</p>
+                            <p className="flex items-center gap-1">
+                                <CiIcon.CiLocationOn size={14} /> Sleman, Daerah Istimewa Yogyakarta
+                            </p>
                             <p>Open Daily, 9 AM - 4 PM</p>
+                        </Columns.Column>
+                        <Columns.Column
+                            width={{ md: 6 }}
+                            className="*:!mb-0 uppercase tracking-0.2 font-semibold text-[1.2rem] text-center lg:text-right">
+                            <div className=" flex items-center justify-end">
+                                <CiIcon.CiInstagram size={30} />
+                                <CiIcon.CiMail size={30} />
+                            </div>
                         </Columns.Column>
                     </Columns.Row>
                 </div>
