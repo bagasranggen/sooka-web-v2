f8d6ee61dea71f50bb9570f5441510fade84c514
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-12-19 15:28:14 +0700

    feature: create offcanvas component

---
+++ src/components/common/Offcanvas/index.ts
@@ -0,0 +1,5 @@
+import { Component } from '@/libs/@types';
+
+import Base, { BaseProps } from '@/components/common/Offcanvas/Base';
+
+export default Object.assign<Component<BaseProps>, {}>(Base, {});
