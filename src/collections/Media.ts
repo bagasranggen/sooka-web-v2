a8b45426747f622096ca40466d2399dc067365c0
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-07-01 06:47:28 +0700

    feature: update media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,16 +1,31 @@
-import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from 'payload';
 
 export const Media: CollectionConfig = {
-  slug: 'media',
-  access: {
-    read: () => true,
-  },
-  fields: [
-    {
-      name: 'alt',
-      type: 'text',
-      required: true,
+    slug: 'media',
+    access: {
+        read: () => true,
     },
-  ],
-  upload: true,
-}
+    fields: [
+        {
+            name: 'alt',
+            type: 'text',
+            required: true,
+        },
+    ],
+    // upload: true,
+    upload: {
+        // staticDir: 'media',
+        imageSizes: [
+            {
+                name: 'productListingThumbnail',
+                width: 400,
+                height: 619,
+            },
+            {
+                name: 'productListingThumbnailMobile',
+                width: 400,
+                height: 400,
+            },
+        ],
+    },
+};
