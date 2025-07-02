74e9cd0fd6a5e2f29b33452f2f58308ca5c396fe
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-07-02 17:09:23 +0700

    refactor: refactoring homepage banner query

--- src/graphql/queries/entries/HomepageQuery.ts
+++ src/graphql/queries/entries/HomepageQuery.ts
@@ -1,42 +1,15 @@
 import { gql } from '@apollo/client';
 
-import { FRAGMENT_TAG } from '@/graphql/queries/entries/fragments/FragmentTag';
-import { FRAGMENT_PRODUCT } from '@/graphql/queries/entries/fragments/FragmentProduct';
-import { FRAGMENT_MEDIA } from '@/graphql/queries/fragments/FragmentMedia';
-import { FRAGMENT_LINK } from '@/graphql/queries/fragments/FragmentLink';
+import { FRAGMENT_HOMEPAGE_BANNER } from '@/graphql/queries/entries/fragments/FragmentHomepageBanner';
 
 export const HOMEPAGE_QUERY = gql`
     query HomepageQuery {
         entry: Homepage {
             bannerMedia {
-                entryStatus
-                source
-                textAlign
-                title
-                description
-                productTarget
-
-                link {
-                    ...bannerCustomLink
-                }
-
-                media {
-                    ...bannerMedia
-                }
-
-                tag {
-                    ...tag
-                }
-
-                product {
-                    ...product
-                }
+                ...banner
             }
         }
     }
 
-    ${FRAGMENT_TAG}
-    ${FRAGMENT_PRODUCT}
-    ${FRAGMENT_MEDIA({ name: 'banner', sizesHandles: ['bannerDesktop', 'bannerTablet', 'bannerMobile'] })}
-    ${FRAGMENT_LINK({ name: 'bannerCustomLink', handle: 'Homepage_BannerMedia_Link' })}
+    ${FRAGMENT_HOMEPAGE_BANNER}
 `;
---
+++ src/graphql/queries/entries/fragments/FragmentHomepageBanner.ts
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client';
+
+import { FRAGMENT_TAG } from '@/graphql/queries/entries/fragments/FragmentTag';
+import { FRAGMENT_PRODUCT } from '@/graphql/queries/entries/fragments/FragmentProduct';
+import { FRAGMENT_MEDIA } from '@/graphql/queries/fragments/FragmentMedia';
+import { FRAGMENT_LINK } from '@/graphql/queries/fragments/FragmentLink';
+
+export const FRAGMENT_HOMEPAGE_BANNER = gql`
+    fragment banner on Homepage_BannerMedia {
+        entryStatus
+        source
+        textAlign
+        title
+        description
+        productTarget
+
+        link {
+            ...bannerCustomLink
+        }
+
+        media {
+            ...bannerMedia
+        }
+
+        tag {
+            ...tag
+        }
+
+        product {
+            ...product
+        }
+    }
+
+    ${FRAGMENT_TAG}
+    ${FRAGMENT_PRODUCT}
+    ${FRAGMENT_MEDIA({ name: 'banner', sizesHandles: ['bannerDesktop', 'bannerTablet', 'bannerMobile'] })}
+    ${FRAGMENT_LINK({ name: 'bannerCustomLink', handle: 'Homepage_BannerMedia_Link' })}
+`;
