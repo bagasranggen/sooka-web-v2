351025f5eb284f5e6285cc66d3c505ca46fd27a7
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-07-03 07:46:08 +0700

    refactor: update media sizes

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -257,6 +257,22 @@ export interface Media {
       filesize?: number | null;
       filename?: string | null;
     };
+    storyMediaDesktop?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+    storyMediaMobile?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
   };
 }
 /**
@@ -625,6 +641,26 @@ export interface MediaSelect<T extends boolean = true> {
               filename?: T;
             };
         productListingThumbnailMobile?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        storyMediaDesktop?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        storyMediaMobile?:
           | T
           | {
               url?: T;
