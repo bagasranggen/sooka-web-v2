89f3193198c1758dc0349253798d062fa4ac7c2d
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-07-04 15:39:44 +0700

    refactor: update utils helper

--- src/libs/factory/contentBlocks/createContentBlocks.ts
+++ src/libs/factory/contentBlocks/createContentBlocks.ts
@@ -2,47 +2,37 @@ import { ArrayString } from '@/libs/@types';
 import { joinArrayString } from '../../utils/joinArrayString';
 import { createContentBlocksSpacing } from './createContentBlocksSpacing';
 
-import { ContentBlocksProps } from '@/components/common/ContentBlocks';
-// import { CONTENT_BLOCKS_DATA_HANDLES } from '@/components/common/ContentBlocks/handlesData';
+import { CONTENT_BLOCKS_DATA_HANDLES } from '../../../components/common/ContentBlocks/handlesData';
 
-export const createContentBlocks = (props: { items: ContentBlocksProps['items'] }) => {
+export const createContentBlocks = (props: { items: any }) => {
     const content: any[] = [];
 
     if (props?.items && props.items.length > 0) {
-        // await Promise.all(
-        //     items.map(async (item, i: number) => {
-        //         let tmp = {
-        //             ...item,
-        //             order: i,
-        //             // locales: locales !== SITE_HANDLES.EN ? locales : LOCALES_HANDLES.EN,
-        //         };
-        //
-        //         // if (page) tmp = { ...tmp, page };
-        //
-        //         // tmp = { ...tmp, ...createContentBlocksSettings(item) };
-        //
-        //         // if (CONTENT_BLOCKS_DATA_HANDLES?.[item?.blockType as keyof typeof CONTENT_BLOCKS_DATA_HANDLES]) {
-        //         //     const data = await CONTENT_BLOCKS_DATA_HANDLES?.[item?.blockType as keyof typeof CONTENT_BLOCKS_DATA_HANDLES](tmp);
-        //         //
-        //         //     tmp = { ...tmp, ...data };
-        //         // }
-        //
-        //         content.push(tmp);
-        //     })
-        // );
+        props.items.forEach((item: any) => {
+            const { cbSpacing, ...props } = item;
 
-        props.items.forEach((item) => {
+            const spacing = createContentBlocksSpacing(cbSpacing);
+
             let className: ArrayString = [];
-
-            const { marginTop, marginBottom, ...props } = item;
-
-            const spacing = createContentBlocksSpacing({ marginTop, marginBottom });
-
             if (spacing) className.push(spacing);
-
             className = joinArrayString(className);
 
-            content.push({ ...props, className });
+            let tmp: any = {
+                ...props,
+                className,
+            };
+
+            const dataProcess =
+                item?.blockType &&
+                CONTENT_BLOCKS_DATA_HANDLES?.[item?.blockType as keyof typeof CONTENT_BLOCKS_DATA_HANDLES];
+
+            if (dataProcess) {
+                const data = dataProcess(tmp);
+
+                tmp = { ...tmp, ...(data as any) };
+            }
+
+            content.push(tmp);
         });
     }
 
