fb021732cc41d257edc9abda9747d75e020e378f
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-07-04 15:37:58 +0700

    refactor: update base content block component

--- src/components/common/ContentBlocks/CbHeading/index.tsx
+++ src/components/common/ContentBlocks/CbHeading/index.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 import Columns from '@/components/common/Columns';
 import Heading, { BaseProps } from '@/components/common/Heading';
 import CbWrapper, { CbWrapperProps } from '@/components/common/ContentBlocks/CbWrapper';
-import CbContainer from '@/components/common/ContentBlocks/CbContainer';
+import CbContainer, { CbContainerProps } from '@/components/common/ContentBlocks/CbContainer';
 
 export type CbHeadingProps = {
     title: BaseProps['children'];
-} & Pick<CbWrapperProps, 'className'>;
+} & (Pick<CbWrapperProps, 'className'> & Pick<CbContainerProps, 'isNested'>);
 
-const CbHeading = ({ title, className }: CbHeadingProps): React.ReactElement => {
+const CbHeading = ({ title, className, isNested }: CbHeadingProps): React.ReactElement => {
     return (
         <CbWrapper className={className}>
-            <CbContainer>
+            <CbContainer isNested={isNested}>
                 <Columns.Row className="justify-center">
                     <Columns.Column width={{ md: 8 }}>
                         <Heading
