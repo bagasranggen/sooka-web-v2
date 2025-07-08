cf4906e2dda7fb2b469422ce70f7cc720d450226
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-07-09 00:58:29 +0700

    refactor: set token access for homepage

--- src/globals/Homepage.ts
+++ src/globals/Homepage.ts
@@ -4,16 +4,49 @@ import { BaseLink, BaseStatus, BaseTarget } from '@/collections/shared';
 export const Homepage: GlobalConfig = {
     slug: 'homepage',
     access: {
-        read: ({ req }) => {
-            const headers = req.headers;
+        read: async ({ req: { headers, payload } }) => {
+            // const headers = req.headers;
             const auth = headers.get('authorization');
+
+            const tokens: string[] = [];
+
+            const tokensDocs = await payload.find({
+                collection: 'tokens' as any,
+            });
+
+            if (tokensDocs?.docs && tokensDocs.docs.length > 0) {
+                tokensDocs.docs.forEach((item: any) => {
+                    console.log(item);
+
+                    if (item?.apiKey) tokens.push(item.apiKey);
+                });
+            }
+
+            console.log({ tokens, auth, auths: tokens.some((item) => auth?.includes(item)) });
+
+            // console.log({ tokens: tokens.docs });
+
+            // console.log({ user, payload });
+
+            // const usersQuery = await payload.find({
+            //     collection: 'users',
+            //     where: {
+            //         code: {
+            //             equals: headers.get('code'),
+            //         },
+            //         secret: {
+            //             equals: headers.get('secret'),
+            //         },
+            //     },
+            // });
+
             //
-            console.log({ headers, auth: auth.includes(process.env.GQL_TOKEN) });
+            // console.log({ headers, auth: auth.includes(process.env.GQL_TOKEN) });
 
             // console.log(args);
 
             // return true;
-            return auth.includes(process.env.GQL_TOKEN);
+            return tokens.some((item) => auth?.includes(item));
         },
     },
     admin: {
--- src/globals/Navigation.ts
+++ src/globals/Navigation.ts
@@ -5,16 +5,50 @@ export const Navigation: GlobalConfig = {
     slug: 'navigation',
     label: 'Header Navigation',
     access: {
-        read: ({ req }) => {
-            const headers = req.headers;
+        read: async ({ req: { headers, payload } }) => {
+            // const headers = req.headers;
             const auth = headers.get('authorization');
+
+            const tokens: string[] = [];
+
+            const tokensDocs = await payload.find({
+                collection: 'tokens' as any,
+            });
+
+            if (tokensDocs?.docs && tokensDocs.docs.length > 0) {
+                tokensDocs.docs.forEach((item: any) => {
+                    console.log(item);
+
+                    if (item?.apiKey) tokens.push(item.apiKey);
+                });
+            }
+
+            console.log({ tokens, auth, auths: tokens.some((item) => auth?.includes(item)) });
+
+            // console.log({ tokens: tokens.docs });
+
+            // console.log({ user, payload });
+
+            // const usersQuery = await payload.find({
+            //     collection: 'users',
+            //     where: {
+            //         code: {
+            //             equals: headers.get('code'),
+            //         },
+            //         secret: {
+            //             equals: headers.get('secret'),
+            //         },
+            //     },
+            // });
+
             //
-            console.log({ headers, auth: auth.includes(process.env.GQL_TOKEN) });
+            // console.log({ headers, auth: auth.includes(process.env.GQL_TOKEN) });
 
             // console.log(args);
 
             // return true;
-            return auth.includes(process.env.GQL_TOKEN);
+            return tokens.some((item) => auth?.includes(item));
+            // return !!auth?.includes(process.env.GQL_TOKEN);
         },
         // read: () => true,
     },
