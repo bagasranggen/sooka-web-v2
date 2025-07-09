6074d7daa2254af8c205767ea441bff66edf951e
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-07-09 09:44:27 +0700

    fix: add vercel bypass protection

--- src/libs/fetcher/apolloClient.ts
+++ src/libs/fetcher/apolloClient.ts
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { Tokens } from '@/collections/Tokens';
 
 const API_URL = process.env.GQL_URL;
 
@@ -14,10 +15,8 @@ const authLink = setContext((_, { headers }) => {
             ...headers,
             'Content-Type': 'application/json',
             Accept: 'application/json',
-
-            // credentials: 'include',
-            // Authorization: `Bearer ${process.env.STRAPI_KEY}`,
-            Authorization: `token API-Key ${process.env.GQL_TOKEN}`,
+            Authorization: `${Tokens.slug} API-Key ${process.env.GQL_TOKEN}`,
+            'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
         },
     };
 });
