4adf9c806b6066e62bd201bd2b0f79263ac12c86
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-07-10 15:54:06 +0700

    refactor: add rewrite to load image from cms

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/api/media/:path*',
+                destination: `${process.env.CMS_URL}/api/media/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
