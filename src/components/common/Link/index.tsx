fd13244ed818299ca497c0d67f17ac6de633e653
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-07-10 16:39:49 +0700

    refactor: update link replace

--- src/components/common/Link/index.tsx
+++ src/components/common/Link/index.tsx
@@ -6,14 +6,25 @@ import { joinArrayString } from '@/libs/utils';
 
 export type LinkProps = Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof NextLinkProps> & NextLinkProps;
 
-const Link = ({ className, ...rest }: LinkProps): React.ReactElement => {
+const Link = ({ className, href, ...rest }: LinkProps): React.ReactElement => {
     let linkClass: ArrayString = ['cursor-pointer focus-visible:outline-0'];
     if (className) linkClass.push(className);
     linkClass = joinArrayString(linkClass);
 
+    const hrefReplacer = (href: NextLinkProps['href']) => {
+        let hrefUpdate: NextLinkProps['href'] = href;
+        const replaceHref = process.env.NEXT_PUBLIC_LINK_REPLACE?.split(',');
+        if (typeof href === 'string' && replaceHref && href.includes(replaceHref[0])) {
+            hrefUpdate = href.replace(replaceHref[0], replaceHref[1]);
+        }
+
+        return hrefUpdate;
+    };
+
     return (
         <NextLink
             className={linkClass}
+            href={hrefReplacer(href)}
             {...rest}
         />
     );
