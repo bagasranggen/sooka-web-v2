5cba1b991aa3a0459eed126134645fa60b3a4e99
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-10-20 00:20:58 +0700

    refactor: add animation on homepage

--- src/components/pages/HomepageIndex/index.tsx
+++ src/components/pages/HomepageIndex/index.tsx
@@ -10,6 +10,7 @@ import HomepageStory, { HomepageStoryProps } from '@/components/pages/HomepageIn
 import RichText, { RichTextProps } from '@/components/common/RichText';
 import HomepageHighlight, { HomepageHighlightProps } from '@/components/pages/HomepageIndex/HomepageHighlight';
 import HomepageBanner, { HomepageBannerProps } from '@/components/pages/HomepageIndex/HomepageBanner';
+import Animation from '@/components/Animation';
 
 export type HomepageIndexProps = {
     entries: {
@@ -29,65 +30,79 @@ const HomepageIndex = ({ entries }: HomepageIndexProps): React.ReactElement => {
         <>
             {entries?.banner && <HomepageBanner items={entries.banner} />}
 
-            {entries?.highlights && <HomepageHighlight items={entries.highlights} />}
+            {entries?.highlights && entries.highlights.length > 0 && (
+                <Animation type="fade-in">
+                    <HomepageHighlight items={entries.highlights} />
+                </Animation>
+            )}
 
-            {entries?.story?.description && <HomepageStory {...entries?.story} />}
+            {entries?.story?.description && (
+                <Animation type="fade-in">
+                    <HomepageStory {...entries?.story} />
+                </Animation>
+            )}
 
             {entries?.testimonials && entries.testimonials.length > 0 && (
-                <Container
-                    as="section"
-                    className="mt-10 md:mt-20">
-                    <Columns.Row className="justify-center">
-                        <Columns.Column
-                            width={{
-                                md: 10,
-                                lg: 9,
-                            }}>
-                            <Heading
-                                as="h2"
-                                size="section"
-                                className="text-center">
-                                Testimonials
-                            </Heading>
+                <Animation type="fade-in">
+                    <Container
+                        as="section"
+                        className="mt-10 md:mt-20">
+                        <Columns.Row className="justify-center">
+                            <Columns.Column
+                                width={{
+                                    md: 10,
+                                    lg: 9,
+                                }}>
+                                <Heading
+                                    as="h2"
+                                    size="section"
+                                    className="text-center">
+                                    Testimonials
+                                </Heading>
 
-                            <div className="mt-8">
-                                <Carousel.Testimonial items={entries.testimonials} />
-                            </div>
-                        </Columns.Column>
-                    </Columns.Row>
-                </Container>
+                                <div className="mt-8">
+                                    <Carousel.Testimonial items={entries.testimonials} />
+                                </div>
+                            </Columns.Column>
+                        </Columns.Row>
+                    </Container>
+                </Animation>
             )}
 
             {entries?.imageDivider && entries.imageDivider.length > 0 && (
-                <ImageDivider
-                    className="mt-10 md:mt-20"
-                    media={entries.imageDivider}
-                />
+                <Animation type="fade-in">
+                    <ImageDivider
+                        className="mt-10 md:mt-20"
+                        media={entries.imageDivider}
+                    />
+                </Animation>
             )}
 
             {entries?.orders?.steps && entries.orders.steps.length > 0 && (
-                <Container
-                    as="section"
-                    className="mt-10 md:mt-20 mb-10 md:mb-20">
-                    <Columns.Row className="justify-center">
-                        <Columns.Column width={{ lg: 8 }}>
-                            <Heading
-                                as="h2"
-                                size="section"
-                                className="text-center"
-                                description={
-                                    <RichText className="text-center">{entries?.orders?.children as any}</RichText>
-                                }>
-                                First Time <span className="text-sooka-primary">Ordering</span>?
-                            </Heading>
+                <Animation type="fade-in">
+                    <Container
+                        as="section"
+                        className="mt-10 md:mt-20 mb-10 md:mb-20">
+                        <Columns.Row className="justify-center">
+                            <Columns.Column width={{ lg: 8 }}>
+                                <Heading
+                                    as="h2"
+                                    size="section"
+                                    className="text-center"
+                                    description={
+                                        <RichText className="text-center">{entries?.orders?.children as any}</RichText>
+                                    }>
+                                    First Time <span className="text-sooka-primary">Ordering</span>?
+                                </Heading>
 
-                            <List.Number
-                                className="mt-8"
-                                items={entries.orders.steps}
-                            />
-                        </Columns.Column>
-                    </Columns.Row>
-                </Container>
+                                <List.Number
+                                    className="mt-8"
+                                    items={entries.orders.steps}
+                                />
+                            </Columns.Column>
+                        </Columns.Row>
+                    </Container>
+                </Animation>
             )}
         </>
     );
