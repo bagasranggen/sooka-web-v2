5f04880a0fdba686fd80b1d305a8d32b9743fe33
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-10-31 22:21:38 +0700

    refactor: add content block marquee

--- src/components/common/ContentBlocks/CbContainer/index.tsx
+++ src/components/common/ContentBlocks/CbContainer/index.tsx
@@ -1,14 +1,23 @@
-import React, { PropsWithChildren } from 'react';
+import React, { ExoticComponent, FragmentProps, PropsWithChildren } from 'react';
 
 import Container from '@/components/common/Container';
 import { CbWrapperProps } from '@/components/common/ContentBlocks/CbWrapper';
+import { ElementTagsProps } from '@/libs/@types';
 
 export type CbContainerProps = {
     isNested?: boolean;
 } & (Pick<CbWrapperProps, 'className'> & PropsWithChildren);
 
 const CbContainer = ({ children, isNested, className }: CbContainerProps): React.ReactElement => {
-    if (isNested) return <>{children}</>;
+    if (isNested) {
+        let Wrapper: ExoticComponent<FragmentProps> | ElementTagsProps = React.Fragment;
+        if (className) Wrapper = 'section';
+
+        let wrapperProps = {};
+        if (className) wrapperProps = { className };
+
+        return <Wrapper {...wrapperProps}>{children}</Wrapper>;
+    }
 
     return (
         <Container
--- src/components/common/ContentBlocks/handles.ts
+++ src/components/common/ContentBlocks/handles.ts
@@ -2,6 +2,7 @@ export const CONTENT_BLOCK_TYPE = {
     CALLOUT: 'callout',
     GALLERY: 'gallery',
     HEADING: 'heading',
+    MARQUEE: 'marquee',
     RELATED_PRODUCTS: 'relatedProducts',
 } as const;
 
@@ -9,5 +10,6 @@ export const CONTENT_BLOCK_INTERFACE = {
     [CONTENT_BLOCK_TYPE.CALLOUT]: 'ContentBlockCallout',
     [CONTENT_BLOCK_TYPE.GALLERY]: 'ContentBlockGallery',
     [CONTENT_BLOCK_TYPE.HEADING]: 'ContentBlockHeading',
+    [CONTENT_BLOCK_TYPE.MARQUEE]: 'ContentBlockMarquee',
     [CONTENT_BLOCK_TYPE.RELATED_PRODUCTS]: 'ContentBlockRelatedProducts',
 } as const;
