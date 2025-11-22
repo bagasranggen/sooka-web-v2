df77cc80d5a6b580fe9a2bcbb9b2fcf1a7302686
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-11-23 00:43:21 +0700

    refactor: update marquee picture

--- src/components/common/Marquee/Picture/index.tsx
+++ src/components/common/Marquee/Picture/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
-import Base from '@/components/common/Marquee';
-import { default as BasePicture, BaseProps } from '@/components/common/Picture';
+import Base, { BaseProps } from '@/components/common/Marquee';
+import { default as BasePicture, BaseProps as BasePictureProps } from '@/components/common/Picture';
 
 export type PictureProps = {
-    items: BaseProps['items'][];
-};
+    items: BasePictureProps['items'][];
+} & Pick<BaseProps, 'config'>;
 
-const Picture = ({ items }: PictureProps): React.ReactElement | null => {
+const Picture = ({ items, config }: PictureProps): React.ReactElement | null => {
     if (!items || items.length === 0) return null;
 
     return (
-        <Base itemClassName="flex flex-nowrap *:ms-2">
-            {items.map((item: BaseProps['items'], i: number) => (
+        <Base
+            config={config}
+            itemClassName="flex flex-nowrap *:ms-2">
+            {items.map((item: BasePictureProps['items'], i: number) => (
                 <BasePicture
                     key={i}
                     items={item}
