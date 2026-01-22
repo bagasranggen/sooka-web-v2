40bcdc3c08b3d702d96b6e45e3db456ebda6a5c9
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-01-22 10:17:19 +0700

    refactor: update input label styling

--- src/components/common/Input/Label/index.tsx
+++ src/components/common/Input/Label/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
 
 import { ArrayString } from '@/libs/@types';
 import { joinArrayString } from '@/libs/utils';
@@ -10,7 +10,9 @@ export type LabelProps = {
 } & (Omit<BaseProps, 'id'> & Required<Pick<BaseProps, 'id'>>);
 
 const Label = forwardRef<HTMLInputElement | HTMLSelectElement, LabelProps>(
-    ({ id, label, className, type, error, hidden, ...props }, ref) => {
+    ({ id, label, className, type, error, hidden, onFocus, onBlurCapture, ...props }, ref) => {
+        const [isFocus, setIsFocus] = useState<boolean>(false);
+
         let inputGroupClass: ArrayString = ['group relative after'];
         inputGroupClass.push("after:content-[''] after:bg-sooka-primary after:transition-width");
         inputGroupClass.push('after:absolute after:left-0 after:bottom-0');
@@ -22,8 +24,10 @@ const Label = forwardRef<HTMLInputElement | HTMLSelectElement, LabelProps>(
 
         let inputClass: ArrayString = ['peer w-full placeholder:opacity-0 bg-transparent focus-visible:outline-none'];
         inputClass.push('min-h-[6.7rem]');
-        if (type !== 'select') inputClass.push('pt-2 px-2');
-        if (type === 'select') inputClass.push('pt-2 px-[1.75rem]');
+        if ((type !== 'select' && !props?.multiple) || (type === 'select' && !props?.multiple)) inputClass.push('pt-2');
+        if (type === 'select' && props?.multiple) inputClass.push('mt-3');
+        if (type !== 'select') inputClass.push('px-2');
+        if (type === 'select') inputClass.push('px-[1.75rem]');
         inputClass.push('text-[2rem]');
         inputClass.push('border-b-2 border-b-sooka-primary/10');
         if (className) inputClass.push(className);
@@ -33,12 +37,13 @@ const Label = forwardRef<HTMLInputElement | HTMLSelectElement, LabelProps>(
         labelClass.push('text-[2.5rem] whitespace-nowrap');
         labelClass.push('tracking-[.6rem]');
         labelClass.push('px-2 py-1');
-        labelClass.push('absolute top-1/2 left-0 -translate-y-1/2');
+        labelClass.push('absolute left-0 top-0');
+        if (!isFocus) labelClass.push('peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2');
         labelClass.push(
-            'group-focus-within:scale-50 group-focus-within:font-semibold group-focus-within:translate-y-[-3rem] group-focus-within:translate-x-[1.2rem]'
+            'group-focus-within:scale-50 group-focus-within:font-semibold group-focus-within:translate-x-[1.2rem]'
         );
         labelClass.push(
-            'peer-[:not(:placeholder-shown)]:scale-50 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:translate-y-[-3rem] peer-[:not(:placeholder-shown)]:translate-x-[1.2rem]'
+            'peer-[:not(:placeholder-shown)]:scale-50 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:translate-x-[1.2rem]'
         );
         labelClass = joinArrayString(labelClass);
 
@@ -52,6 +57,14 @@ const Label = forwardRef<HTMLInputElement | HTMLSelectElement, LabelProps>(
                         className={inputClass}
                         placeholder={label}
                         hidden={hidden}
+                        onFocus={(e: any) => {
+                            setIsFocus(true);
+                            if (onFocus) onFocus(e);
+                        }}
+                        onBlurCapture={(e: any) => {
+                            setIsFocus(false);
+                            if (onBlurCapture) onBlurCapture(e);
+                        }}
                         {...props}
                     />
 
