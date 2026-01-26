8245290f037c10a8577bad6457a942958068f835
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-01-26 14:12:08 +0700

    refactor: refactor overlay component

--- src/components/common/Overlay/index.tsx
+++ src/components/common/Overlay/index.tsx
@@ -7,24 +7,18 @@ export type OverlayProps = {
     variant: 'solid' | 'gradient-left' | 'gradient-right';
     opacity: NumericRange<CreateArrayWithLengthX<0>, 6>;
     children: React.ReactElement;
-    // children: React.Component<React.HTMLAttributes<HTMLElement>>;
 };
 
 const Overlay = ({ variant, opacity, children }: OverlayProps) => {
-    let overlayClass: ArrayStringProps = [
-        'relative after:content-[""] after:absolute after:top-0 after:left-0 after:w-full after:h-full',
-    ];
-    if (variant !== 'solid') overlayClass.push('after:from-black');
-    if (variant === 'solid') overlayClass.push('after:bg-black');
-    if (variant === 'gradient-right') overlayClass.push('after:bg-linear-to-l');
-    if (variant === 'gradient-left') overlayClass.push('after:bg-linear-to-r');
-    overlayClass.push(`after:opacity-${opacity * 10}`);
+    let overlayClass: ArrayStringProps = ['overlay'];
+    if (variant === 'gradient-right') overlayClass.push('overlay--gradient-right');
+    if (variant === 'gradient-left') overlayClass.push('overlay--gradient-left');
+    overlayClass.push(`overlay--${opacity * 10}`);
     overlayClass = joinArrayString(overlayClass);
 
     const props: any = children.props;
 
     let childClass: ArrayStringProps = [overlayClass];
-    // if (props?.className) childClass.push(props.className);
     if (props?.className) childClass.push(props.className);
     childClass = joinArrayString(childClass);
 
