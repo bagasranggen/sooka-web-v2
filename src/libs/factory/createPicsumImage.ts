30879fd05ab7900cc68d574b6a13103f0b5d5bdc
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-07-09 22:31:30 +0700

    refactor: update mock component

--- src/libs/factory/createPicsumImage.ts
+++ src/libs/factory/createPicsumImage.ts
@@ -2,12 +2,14 @@ export const createPicsumImage = ({
     id = 237,
     width = 400,
     height = 619,
+    media,
     alt,
-}: { alt?: string } & Partial<Record<'id' | 'width' | 'height', number>>) => {
+}: { alt?: string; media?: number } & Partial<Record<'id' | 'width' | 'height', number>>) => {
     return {
         src: `https://picsum.photos/id/${id}/${width}/${height}`,
         width,
         height,
         alt: alt ?? `image ${id}`,
+        media: media ?? undefined,
     };
 };
--- src/libs/mock/card.ts
+++ src/libs/mock/card.ts
@@ -1,17 +1,17 @@
 import { createArrayFromNumber } from '@/libs/factory/createArrayFromNumber';
-// import { createPicsumImage, createPictureImage } from '@/libs/factory';
+import { createPicsumImage } from '@/libs/factory/createPicsumImage';
 
 export const CARD_THUMBNAIL_WITH_PRICE = createArrayFromNumber(4).map((item) => ({
     cta: {
         href: '/cakes/strawberry-shortcake',
     },
     media: [
-        // createPictureImage({ item: createPicsumImage({ id: 237, width: 400, height: 619 }), media: 768 }),
-        // createPictureImage({ item: createPicsumImage({ id: 237, width: 400, height: 400 }) }),
+        createPicsumImage({ id: 237, width: 400, height: 619, media: 768 }),
+        createPicsumImage({ id: 237, width: 400, height: 400 }),
     ],
     mediaHover: [
-        // createPictureImage({ item: createPicsumImage({ id: 22, width: 400, height: 619 }), media: 768 }),
-        // createPictureImage({ item: createPicsumImage({ id: 22, width: 400, height: 400 }) }),
+        createPicsumImage({ id: 22, width: 400, height: 619, media: 768 }),
+        createPicsumImage({ id: 22, width: 400, height: 400 }),
     ],
     title: 'Strawberry Shortcake',
     price: item === 0 ? '500.000' : '230.000',
