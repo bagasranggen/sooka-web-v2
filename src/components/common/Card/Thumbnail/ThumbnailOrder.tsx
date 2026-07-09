a200992eaa693586f5274313822a4f09b7148c44
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-07-09 22:31:18 +0700

    refactor: separate card thumbnail component

---
+++ src/components/common/Card/Thumbnail/ThumbnailOrder.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export type ThumbnailOrderProps = {};
+
+const ThumbnailOrder = ({}: ThumbnailOrderProps): React.ReactElement => {
+    return <>ORDER</>;
+};
+
+export default ThumbnailOrder;
