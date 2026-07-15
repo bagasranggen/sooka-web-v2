4c34720c9dea2e23890019994ce77f96bb95f8ae
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-07-15 14:38:06 +0700

    refactor: add order context

--- src/store/context/root.ts
+++ src/store/context/root.ts
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 
 import { GlobalStateContext } from '@/store/context/GlobalContext';
 import { HistoryStateContext } from '@/store/context/HistoryContext';
+import { OrderStateContext } from '@/store/context/OrderContext';
 
 export const useGlobalStateContext = () => useContext(GlobalStateContext);
 export const useHistoryStateContext = () => useContext(HistoryStateContext);
+export const useOrderStateContext = () => useContext(OrderStateContext);
