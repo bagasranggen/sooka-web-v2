2d46b2e42665796356dc19e13cb47bbf95f51067
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-07-15 14:37:47 +0700

    refactor: add hooks

---
+++ src/libs/hooks/useDebounceWindowSize.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+import { useDebounce, useWindowSize } from 'react-use';
+
+export const useDebounceWindowSize = (ms: number = 250) => {
+    const { width, height } = useWindowSize();
+
+    const [debounceWidth, setDebounceWidth] = useState<number>(0);
+    const [debounceHeight, setDebounceHeight] = useState<number>(0);
+
+    const [] = useDebounce(
+        () => {
+            setDebounceWidth(width);
+            setDebounceHeight(height);
+        },
+        ms,
+        [width, height]
+    );
+
+    return { width: debounceWidth, height: debounceHeight };
+};
