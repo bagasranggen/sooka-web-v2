fc574ed0f8996c44ab8800285662a04d06decfef
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-07-17 13:53:30 +0700

    refactor: add modal cart

---
+++ src/components/common/Modal/Cart/index.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import React, { Ref } from 'react';
+
+import { useMeasure } from 'react-use';
+
+import Form, { CartProps as FormCartProps } from '@/components/common/Form';
+import Columns from '@/components/common/Columns';
+import Heading from '@/components/common/Heading';
+import Button from '@/components/common/Button';
+import ModalSheet, { ModalSheetProps } from '@/components/common/Modal/ModalSheet';
+
+export type CartProps = {} & Pick<ModalSheetProps, 'open' | 'onOpenChange'> & Pick<FormCartProps, 'items' | 'onSubmit'>;
+
+const Cart = ({ open, onOpenChange, items, onSubmit }: CartProps): React.ReactElement => {
+    const [ref, { height }] = useMeasure();
+
+    return (
+        <ModalSheet
+            open={open}
+            onOpenChange={onOpenChange}
+            className="modal--cart"
+            modal={{
+                showCloseButton: false,
+            }}
+            sheet={{
+                showCloseButton: false,
+                side: 'bottom',
+            }}>
+            <Columns gutterY={3}>
+                <Columns.Column
+                    style={{ '--sticky-height': `${height}px` } as React.CSSProperties}
+                    className="max-lg:pb-[calc(var(--sticky-height))]"
+                    md={12}
+                    lg={9}
+                    xl={10}>
+                    <Form.Cart
+                        items={items}
+                        onSubmit={onSubmit}
+                    />
+                </Columns.Column>
+
+                <Columns.Column
+                    md={12}
+                    lg={3}
+                    xl={2}>
+                    <div
+                        ref={ref as Ref<HTMLDivElement>}
+                        className="modal__sticky">
+                        <Columns
+                            className="max-lg:items-center"
+                            gutterY={{ xs: 0, md: 3 }}>
+                            <Columns.Column
+                                xs={8}
+                                lg={12}>
+                                <Heading
+                                    as="h2"
+                                    className="text-[3rem] leading-3.5">
+                                    Total
+                                </Heading>
+                                <p className="text-md uppercase tracking-0.1 font-bold">Rp1.200.000</p>
+                            </Columns.Column>
+
+                            <Columns.Column
+                                xs={4}
+                                lg={12}
+                                className="text-end lg:text-center">
+                                <Button.Arrow as="button">ORDER</Button.Arrow>
+                            </Columns.Column>
+                        </Columns>
+                    </div>
+                </Columns.Column>
+            </Columns>
+        </ModalSheet>
+    );
+};
+
+export default Cart;
--- src/components/common/Modal/index.ts
+++ src/components/common/Modal/index.ts
@@ -1,15 +1,19 @@
 import { Component } from '@/libs/@types';
 
 import Base, { BaseProps } from '@/components/common/Modal/Base';
+import Cart, { CartProps } from '@/components/common/Modal/Cart';
 import Purchase, { PurchaseProps } from '@/components/common/Modal/Purchase';
 import ModalSheet, { ModalSheetProps } from '@/components/common/Modal/ModalSheet';
 
 export type * from '@/components/common/Modal/Base';
+export type * from '@/components/common/Modal/Cart';
 export type * from '@/components/common/Modal/Purchase';
+export type * from '@/components/common/Modal/ModalSheet';
 
 type ModalComposition = {
+    Cart: Component<CartProps>;
     Purchase: Component<PurchaseProps>;
     ModalSheet: Component<ModalSheetProps>;
 };
 
-export default Object.assign<Component<BaseProps>, ModalComposition>(Base, { Purchase, ModalSheet });
+export default Object.assign<Component<BaseProps>, ModalComposition>(Base, { Cart, Purchase, ModalSheet });
