65d70fc65e508e5ec884fe78c7d1b42468c4c9a5
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-07-29 17:14:35 +0700

    refactor: convert container component to be able to accept ref

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -1,18 +1,31 @@
-import React, { PropsWithChildren } from 'react';
+import React, { forwardRef, PropsWithChildren } from 'react';
 
 import { ArrayStringProps, ElementTagsProps, ClassnameProps } from '@/libs/@types';
 import { joinArrayString } from '@/libs/utils';
 
+export type ContainerRefProps = HTMLElement;
+
 export type ContainerProps = {
     as?: Extract<ElementTagsProps, 'section' | 'div'>;
+    fluid?: boolean;
 } & (PropsWithChildren & ClassnameProps);
 
-const Container = ({ as: BlockContainer = 'div', className, children }: ContainerProps): React.ReactElement => {
-    let containerClass: ArrayStringProps = ['container'];
-    if (className) containerClass.push(className);
-    containerClass = joinArrayString(containerClass);
+const Container = forwardRef<ContainerRefProps, ContainerProps>(
+    ({ as: BlockContainer = 'div', fluid, className, children }, ref) => {
+        let containerClass: ArrayStringProps = fluid ? ['container-fluid'] : ['container'];
 
-    return <BlockContainer className={containerClass}>{children}</BlockContainer>;
-};
+        if (className) containerClass.push(className);
+        containerClass = joinArrayString(containerClass);
 
+        return (
+            <BlockContainer
+                ref={ref as any}
+                className={containerClass}>
+                {children}
+            </BlockContainer>
+        );
+    }
+);
+
+Container.displayName = 'Container';
 export default Container;
