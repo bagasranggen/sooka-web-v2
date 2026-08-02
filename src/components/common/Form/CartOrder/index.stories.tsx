3de5f49ae12dcc7ab1a1141144f10df147ffbb65
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-08-02 17:01:30 +0700

    refactor: update cart order form stories

--- src/components/common/Form/CartOrder/index.stories.tsx
+++ src/components/common/Form/CartOrder/index.stories.tsx
@@ -38,10 +38,11 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
     args: {
-        priceCurrency: 'Rp 2.440.000',
         // items: FORM_CART,
         // onSubmit: (data) => {
         //     console.log({ data });
         // },
+        priceCurrency: 'Rp 2.440.000',
+        note: 'This is a note',
     },
 };
